0a4fa097165d5315677de9adf34cb6350bb64f05
ngoiefranssen <ngoiefranssen04@gmail.com>
2023-09-05 21:42:56 -0400

    Some changes

--- src/AppStore.js
+++ src/AppStore.js
@@ -6,5 +6,7 @@ const appStore = set => ({
   updateOpen: dopen => set(state => ({ dopen: dopen })),
 });
 
-appStore = persist(appStore, { name: 'my_app_store' });
+const appStore_ = persist(appStore, { name: 'my_app_store' });
 export const useAppStore = create(appStore);
+
+// 12:16
--- src/view/pages/components/Home.js
+++ src/view/pages/components/Home.js
@@ -1,7 +1,9 @@
-import { Box, Grid } from '@mui/material';
+import { Box, Card, CardContent, Grid, Stack, Typography } from '@mui/material';
 import React from 'react';
 import SideNav from '../layouts/SideNav';
 import NavBar from '../layouts/NavBar';
+import '../../styles/Dash.css';
+import StorefrontIcon from '@mui/icons-material/Storefront';
 // const SideNav = React.lazy(() => import('../layouts/SideNav'));
 
 const Home = () => {
@@ -13,22 +15,75 @@ const Home = () => {
         <SideNav />
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
           <Grid container spacing={2}>
-            <Grid item xs={8}></Grid>
-            <Grid item xs={4}></Grid>
+            <Grid item xs={8}>
+              <Stack spacing={2} direction="row">
+                <Card sx={{ maxWidth: '49%', height: 140 }}>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Lizards are a widespread group of squamate reptiles, with
+                      over 6,000 species, ranging across all continents except
+                      Antarctica
+                    </Typography>
+                  </CardContent>
+                </Card>
+                <Card sx={{ maxWidth: '49%', height: 140 }}>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Lizards are a widespread group of squamate reptiles, with
+                      over 6,000 species, ranging across all continents except
+                      Antarctica
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Stack>
+            </Grid>
+            <Grid item xs={4}>
+              <Stack spacing={2}>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardContent>
+                    <Stack spacing={2} direction="row">
+                      <div className="icon_style">
+                        <StorefrontIcon />
+                      </div>
+                      <div className="allpadding">
+                        <span className="title_price">$273k</span>
+                        <br />
+                        <span className="title_sub_price">Total incomes</span>
+                      </div>
+                    </Stack>
+                  </CardContent>
+                </Card>
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardContent></CardContent>
+                </Card>
+              </Stack>
+            </Grid>
           </Grid>
 
           <Box height={20} />
 
           <Grid container spacing={2}>
-            <Grid item xs={8}></Grid>
-            <Grid item xs={4}></Grid>
+            <Grid item xs={8}>
+              <Card sx={{ height: 60 + 'vh' }}>
+                <CardContent></CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={4}>
+              <Card sx={{ height: 60 + 'vh' }}>
+                <CardContent></CardContent>
+              </Card>
+            </Grid>
           </Grid>
         </Box>
       </Box>
     </>
   );
 };
-
-// 1:50 min
 
 export default Home;
