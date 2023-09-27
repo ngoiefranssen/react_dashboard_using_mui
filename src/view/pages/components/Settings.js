b87b665c716a126f8530276e719eb2cc7c692c88
ngoiefranssen <ngoiefranssen04@gmail.com>
2023-09-26 21:17:15 -0400

    Some change

--- src/view/pages/components/Settings.js
+++ src/view/pages/components/Settings.js
@@ -2,17 +2,18 @@ import { Box } from '@mui/material';
 import React from 'react';
 import SideNav from '../layouts/SideNav';
 import NavBar from '../layouts/NavBar';
+import Listing from '../settings/Listing';
 // const SideNav = React.lazy(() => import('../layouts/SideNav'));
 
 const Settings = () => {
   return (
     <>
       <NavBar />
-      <Box height={30} />
+      <Box height={70} />
       <Box sx={{ display: 'flex' }}>
         <SideNav />
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <h1>Settings.....</h1>
+          <Listing />
         </Box>
       </Box>
     </>
